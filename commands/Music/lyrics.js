ca4895c4f6e2695e72085c9081d0af661e797fac
TheMonDon <bforster48@gmail.com>
2020-08-17 10:59:52 -0500

    fix lyrics

--- commands/Music/lyrics.js
+++ commands/Music/lyrics.js
@@ -15,14 +15,14 @@ class Lyrics extends Command {
   async run (msg, args) {
     let song;
     if (!args || args.length < 1) {
-      song = msg.client.player.getQueue(msg.guild.id).playing.name;
+      song = msg.client.player.getQueue(msg.guild.id) && msg.client.player.getQueue(msg.guild.id).playing.name;
       if (!song) return msg.channel.send('I can\'t get the lyrics of nothing.');
     } else {
       song = args.join(' ');
     }
     const lyrics = await lf(song, '');
     if (!lyrics) return msg.channel.send(`No lyrics found for: ${song}`);
-    
+
     msg.channel.send(lyrics, { code: 'ascii', split: '\n'});
 
   }
