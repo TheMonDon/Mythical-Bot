e1d600477dad79d72f0364c9aa1d1c199ac95bfb
TheMonDon <unconfigured@null.spigotmc.org>
2020-12-03 13:36:14 -0600

    re-add restart command

---
+++ commands/Bot Admin/restart.js
@@ -0,0 +1,26 @@
+const Command = require('../../base/Command.js');	
+
+class Restart extends Command {	
+  constructor (client) {	
+    super(client, {	
+      name: 'restart',	
+      description: 'Restarts the bot',	
+      category: 'Bot Admin',	
+      usage: 'restart',	
+      permLevel: 'Bot Admin',
+      aliases: ['reboot']
+    });	
+  }	
+
+  async run (message, args, level) { // eslint-disable-line no-unused-vars	
+    try {	
+      await message.reply('Bot is restarting down.');	
+      await Promise.all(this.client.commands.map(cmd => this.client.unloadCommand(cmd)));	
+      process.exit(0);	
+    } catch (e) {	
+      console.log(e);	
+    }	
+  }	
+}	
+
+module.exports = Restart;
