e84340cd2793365a441fe0ef324bfbb60613cbbe
TheMonDon <unconfigured@null.spigotmc.org>
2021-02-21 01:20:00 -0600

    Added server stats to mctodc

--- commands/Fun/mctodc.js
+++ commands/Fun/mctodc.js
@@ -1,8 +1,8 @@
 const Command = require('../../base/Command.js');
 const DiscordJS = require('discord.js');
-const request = require('request'); // request is depreciated i will find something else later
 const fetch = require('node-superfetch');
 const { JSONPath } = require('jsonpath-plus');
+const request = require('request'); // request is depreciated i will find something else later
 const mysql = require('mysql');
 
 class mctodc extends Command {
@@ -15,15 +15,26 @@ class mctodc extends Command {
     });
   }
 
-  async run(msg, text) { // eslint-disable-line no-unused-vars
+  async run(msg, text) {
+    const server = msg.guild;
+
     let user;
+    let user1;
     if (!text || text.length < 1) {
-      user = msg.author.id;
+      user1 = msg.member;
+      user = user1.id;
     } else {
-      user = msg.mentions.members.first() || server.members.cache.find(m => m.id === `${text.join(' ')}`) || server.members.cache.find(m => m.displayName.toUpperCase() === `${text.join(' ').toUpperCase()}`) || server.members.cache.find(m => m.user.username.toUpperCase() === `${text.join(' ').toUpperCase()}`) || server.members.cache.find(m => m.user.username.toLowerCase()
-        .includes(`${text.join(' ').toLowerCase()}`)) || msg.member;
-      user = user.id
+      user1 = msg.mentions.members.first() || server.members.cache.find(m => m.id === `${text.join(' ')}`) || server.members.cache.find(m => m.displayName.toUpperCase() === `${text.join(' ').toUpperCase()}`) || server.members.cache.find(m => m.user.username.toUpperCase() === `${text.join(' ').toUpperCase()}`) || server.members.cache.find(m => m.user.username.toLowerCase()
+        .includes(`${text.join(' ').toLowerCase()}`));
+      if (user1) {
+        user = user1.id;
+      } else {
+        user = text.join(' ').trim().replace(/\'/g, '');
+      }
     }
+
+    let member;
+    server.members.cache.get(user) ? member = true : member = false;
 
     const connection = mysql.createConnection({
       host: 'localhost',
@@ -32,35 +43,96 @@ class mctodc extends Command {
       database: 'ranksync'
     });
 
+    const crDB = mysql.createConnection({
+      host: 'localhost',
+      user: 'craftersisland',
+      password: 'Jmonahan13',
+      database: 'chatreaction'
+    });
+
+    const fDB = mysql.createConnection({
+      host: 'localhost',
+      user: 'craftersisland',
+      password: 'Jmonahan13',
+      database: 'friends'
+    });
+
+    fDB.connect();
+    crDB.connect();
     connection.connect();
 
-    connection.query(`SELECT player_id FROM synced_players WHERE identifier = ${user}`, function (error, results, fields) {
-      if (error) return msg.channel.send(`I could not find that user, did they sync their account on the server?`);
-      const player_id = results[0].player_id;
-      connection.query(`SELECT * FROM player WHERE id = ${player_id}`, async function (error, results, fields) {
-        if (error) return msg.channel.send(`I could not find that user, did they sync their account on the server?`);
-        const id = results[0].uuid;
+    const errMsg = "I could not find that user. Did they sync their accounts using `!link`? \nAdd '' around mc username if their discord name is the same.";
 
-        try {
-          const { body } = await fetch.get(`https://api.mojang.com/user/profiles/${id}/names`);
-          const json = body;
-          const nc = JSONPath({ path: '*.name', json }).join(', ');
+    if (!member) {
+      request({
+        url: `https://api.mojang.com/users/profiles/minecraft/${user}`,
+        json: true
+      }, async function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          const uuid = body.id
+          const id = uuid.substr(0,8) + "-" + uuid.substr(8,4) + "-" + uuid.substr(12,4) + "-" + uuid.substr(16,4) + "-" + uuid.substr(20);
+
+          return information(id, crDB, fDB, connection, member, user1, msg);
+        } else {
           const em = new DiscordJS.MessageEmbed()
-            .setTitle(`${nc.slice(nc.lastIndexOf(',') + 1)}'s Account Information`)
-            .setColor('00FF00')
-            .setImage(`https://mc-heads.net/body/${id}`)
-            .addField('Name Changes History', nc || 'Error fetching data...', false)
-            .addField('UUID', id, false)
-            .addField('NameMC Link', `Click [here](https://es.namemc.com/profile/${id}) to go to their NameMC Profile`, false);
-          msg.channel.send(em);
-        } catch (err) {
-          console.error(err);
+            .setTitle('Account Not Found')
+            .setColor('FF0000')
+            .setDescription(`An account with the name \`${user}\` was not found.`);
+          return msg.channel.send(em);
         }
-
-        connection.end();
       });
-    });
+    } else {
+      connection.query(`SELECT player_id FROM synced_players WHERE identifier = ${user}`, function (error, results, fields) {
+        const player_id = results && results[0] && results[0].player_id;
+        if (error || !player_id) return msg.channel.send(errMsg);
+
+        connection.query(`SELECT * FROM player WHERE id = ${player_id}`, async function (error, results, fields) {
+          if (error) return msg.channel.send(errMsg);
+          const id = results && results[0].uuid;
+
+          information(id, crDB, fDB, connection, member, user1, msg);
+        });
+      });
+    }
   }
 }
+
+const information = async function (id, crDB, fDB, connection, member, user1, msg) {
+  try {
+    const { body } = await fetch.get(`https://api.mojang.com/user/profiles/${id}/names`);
+    const json = body;
+    const nc = JSONPath({ path: '*.name', json }).join(', ');
+    const name = nc.slice(nc.lastIndexOf(',') + 1);
+
+    crDB.query(`SELECT * FROM survival_newreactionstats WHERE uuid = '${id}'`, function (error, results, fields) {
+      if (error) console.log(error);
+      const wins = results && results[0] && results[0].wins || 0;
+
+      fDB.query(`SELECT * FROM fr_players WHERE player_uuid = '${id}'`, function (error, results, fields) {
+        if (error) console.log(error);
+        const last_online = results && results[0] && results[0].last_online.toString() || 'Never Joined';
+
+        const em = new DiscordJS.MessageEmbed()
+          .setTitle(`${name}'s Account Information`)
+          .setColor('00FF00')
+          .setImage(`https://mc-heads.net/body/${id}`)
+          .addField('Name Changes History', nc || 'Error fetching data...', false)
+          .addField('UUID', id, false)
+          .addField('NameMC Link', `Click [here](https://es.namemc.com/profile/${id}) to go to their NameMC Profile`, false);
+        if (member) em.addField('Discord', `${user1.user.tag} \(${user1.id}\)`, false);
+        em.addField('Reaction Wins', wins, false);
+        em.addField('Last Online', last_online, false);
+
+        crDB.end();
+        fDB.end();
+        connection.end();
+
+        return msg.channel.send(em);
+      });
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = mctodc;
