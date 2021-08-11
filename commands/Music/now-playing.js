9dae3b9404f2ac5d5187e0f0a21e69a87afcf9a3
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2021-08-10 23:14:06 -0500

    I am blind. This should be it tho

--- commands/Music/now-playing.js
+++ commands/Music/now-playing.js
@@ -24,7 +24,7 @@ class nowPlaying extends Command {
 
     const count = song.duration.match(/:/g) || [];
     let songTime;
-    count.length === 1 ? songTime = `00:${song.duratiom}` : songTime = song.duration;
+    count.length === 1 ? songTime = `00:${song.duration}` : songTime = song.duration;
     const duration = moment.duration(songTime).asSeconds() * 1000;
     const totalTime = queue.currentStreamTime;
     const timeLeft = moment.duration(duration - totalTime).format('d [days] h [hours] m [minutes] s [seconds]');
