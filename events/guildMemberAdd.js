42264b840d06501e9ad3b8f8fa42514b3bdfc224
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2022-08-09 14:12:10 -0500

    fix member join event

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -62,7 +62,7 @@ module.exports = class {
     const welcomeMessage = settings.welcomeMessage.replace('{{user}}', member.user.tag).replace('{{guild}}', member.guild.name);
 
     const em = new EmbedBuilder()
-      .setColor('RANDOM')
+      .setColor('#0099CC')
       .setTitle(`Welcome to ${member.guild.name}`)
       .setAuthor({ name: member.user.tag, iconURL: member.user.displayAvatarURL() })
       .setDescription(welcomeMessage)
