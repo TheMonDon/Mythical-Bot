de4094c39ed9acaa2b88151de93270b84a9a5197
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2023-01-12 00:31:05 -0600

    Eval updates

--- commands/Owner/eval.js
+++ commands/Owner/eval.js
@@ -24,6 +24,7 @@ class Eval extends Command {
     const server = msg.guild;
     const member = msg.member;
     const config = this.client.config;
+    let promise = false;
 
     const embed = new EmbedBuilder()
       .setFooter({ text: msg.author.tag, iconURL: msg.author.displayAvatarURL() });
@@ -31,31 +32,31 @@ class Eval extends Command {
     const query = args.join(' ');
     const code = (lang, code) => (`\`\`\`${lang}\n${String(code).slice(0, 4000) + (code.length >= 4000 ? '...' : '')}\n\`\`\``);
 
-    if (!query) msg.channel.send('Please, write something so I can evaluate!');
-    else {
-      try {
-        const evald = eval(query);
-        const res = typeof evald === 'string' ? evald : inspect(evald, { depth: 0 });
-        const res2 = await clean(this.client, res);
+    if (!query) return msg.channel.send('Please, write something so I can evaluate!');
 
-        embed.setDescription(`Result \n ${code('js', res2)}`);
+    try {
+      let evald = eval(query);
+      if (evald instanceof Promise) evald = await evald; promise = true;
+      const res = typeof evald === 'string' ? evald : inspect(evald, { depth: 0 });
+      const res2 = await clean(this.client, res);
 
-        if (!res || (!evald && evald !== 0)) embed.setColor(msg.settings.embedErrorColor);
-        else {
-          embed
-            .addFields([{ name: 'Type', value: code('css', typeof evald) }])
-            .setColor(msg.settings.embedSuccessColor);
-        }
-      } catch (error) {
+      embed.setDescription(`Result ${promise ? '(Promise Awaited)' : ''} \n ${code('js', res2)}`);
+
+      if (!res || (!evald && evald !== 0)) embed.setColor(msg.settings.embedErrorColor);
+      else {
         embed
-          .addFields([{ name: 'Error', value: code('js', error) }])
-          .setColor(msg.settings.embedErrorColor);
-      } finally {
-        msg.channel.send({ embeds: [embed] })
-          .catch(error => {
-            msg.channel.send(`There was an error while displaying the eval result! ${error.message}`);
-          });
+          .addFields([{ name: 'Type', value: code('css', typeof evald) }])
+          .setColor(msg.settings.embedSuccessColor);
       }
+    } catch (error) {
+      embed
+        .addFields([{ name: 'Error', value: code('js', error) }])
+        .setColor(msg.settings.embedErrorColor);
+    } finally {
+      msg.channel.send({ embeds: [embed] })
+        .catch(error => {
+          msg.channel.send(`There was an error while displaying the eval result! ${error.message}`);
+        });
     }
   }
 }
