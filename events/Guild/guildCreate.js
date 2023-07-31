2191a1d0584bfae16761faf26e76279e2eea4ab2
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2023-07-31 16:10:54 -0500

    Update d.js 14.12, try/catch guilld create/delete

--- events/Guild/guildCreate.js
+++ events/Guild/guildCreate.js
@@ -4,11 +4,18 @@ module.exports = class {
   }
 
   async run(guild) {
-    this.client.user.setActivity(
-      `${this.client.settings.get('default').prefix}help | ${this.client.guilds.cache.size} Servers`,
-    );
-    this.client.logger.log(
-      `New guild has been joined: ${guild.name} (${guild.id}) with ${guild.memberCount - 1} members`,
-    );
+    // Wait 1 second to try and solve guild somehow being undefined
+    this.client.util.wait(1000);
+
+    try {
+      this.client.user.setActivity(
+        `${this.client.settings.get('default').prefix}help | ${this.client.guilds.cache.size} Servers`,
+      );
+      this.client.logger.log(
+        `New guild has been joined: ${guild.name} (${guild.id}) with ${guild.memberCount - 1} members`,
+      );
+    } catch (error) {
+      this.client.logger.error(`GuildCreate: ${error}`);
+    }
   }
 };
--- events/Guild/guildDelete.js
+++ events/Guild/guildDelete.js
@@ -5,12 +5,18 @@ module.exports = class {
 
   async run(guild) {
     if (!guild.available) return; // Ignore unavailable guilds
+    // Wait 1 second to try and solve guild somehow being undefined
+    this.client.util.wait(1000);
 
-    this.client.user.setActivity(
-      `${this.client.settings.get('default').prefix}help | ${this.client.guilds.cache.size} Servers`,
-    );
-    // Well they're gone. Let's remove them from the settings and log it!
-    this.client.settings.delete(guild.id);
-    this.client.logger.log(`Left guild: ${guild.name} (${guild.id}) with ${guild.memberCount} members`);
+    try {
+      this.client.user.setActivity(
+        `${this.client.settings.get('default').prefix}help | ${this.client.guilds.cache.size} Servers`,
+      );
+      // Well they're gone. Let's remove them from the settings and log it!
+      this.client.settings.delete(guild.id);
+      this.client.logger.log(`Left guild: ${guild.name} (${guild.id}) with ${guild.memberCount} members`);
+    } catch (error) {
+      this.client.logger.error(`GuildDelete: ${error}`);
+    }
   }
 };
