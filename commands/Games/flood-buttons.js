260ca62fca3ff23c89c17244f9faba1836bbd707
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2023-08-18 19:51:59 -0500

    Push flood-buttons to production

--- commands/Games/flood-buttons.js
+++ commands/Games/flood-buttons.js
@@ -1,10 +1,8 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js');
 const Command = require('../../base/Command.js');
+const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
 const { QuickDB } = require('quick.db');
 const { Duration } = require('luxon');
 const db = new QuickDB();
-
-// I left this alone on December 9th, unable to figure out how to get buttons to work :(
 
 class FloodButtons extends Command {
   constructor(client) {
@@ -12,14 +10,16 @@ class FloodButtons extends Command {
       name: 'flood-buttons',
       description: 'Play a game of flood.',
       usage: 'flood-buttons',
-      category: 'Administrator',
-      permLevel: 'Administrator',
+      category: 'Games',
+      aliases: ['floodbuttons', 'floodbutton'],
     });
   }
 
   async run(msg) {
     const WIDTH = 13;
     const HEIGHT = 13;
+    const moves = 25;
+
     const SQUARES = {
       red_square: '🟥',
       blue_square: '🟦',
@@ -27,29 +27,43 @@ class FloodButtons extends Command {
       purple_square: '🟪',
       green_square: '🟩',
     };
-    const gameBoard = [];
-    let turn = 0;
-    let message;
-    let gameOver = false;
-    let result;
+    const nameToEmoji = {
+      ':red_square:': '🟥',
+      ':blue_square:': '🟦',
+      ':orange_square:': '🟧',
+      ':purple_square:': '🟪',
+      ':green_square:': '🟩',
+      ':octagonal_sign:': '🛑',
+    };
+
+    const color = msg.settings.embedColor;
     const gameStart = msg.createdAt;
+    const gameBoard = [];
+    let gameOver = false;
+    let turn = 0;
+    let lastMove;
+    let message;
     let gameEnd;
+    let result;
+    let collected;
 
     const current = this.client.games.get(msg.channel.id);
     if (current) return msg.reply(`Please wait until the current game of \`${current.name}\` is finished.`);
-    // this.client.games.set(msg.channel.id, { name: this.help.name, user: msg.author.id });
+    this.client.games.set(msg.channel.id, { name: this.help.name, user: msg.author.id });
 
     const up = (pos) => ({ x: pos.x, y: pos.y - 1 });
     const down = (pos) => ({ x: pos.x, y: pos.y + 1 });
     const left = (pos) => ({ x: pos.x - 1, y: pos.y });
     const right = (pos) => ({ x: pos.x + 1, y: pos.y });
 
+    // Randomly generate the game board
     for (let y = 0; y < HEIGHT; y++) {
       for (let x = 0; x < WIDTH; x++) {
         gameBoard[y * WIDTH + x] = Object.values(SQUARES)[Math.floor(Math.random() * Object.keys(SQUARES).length)];
       }
     }
 
+    // Return the game board as a string
     function gameBoardToString() {
       let str = '';
       for (let y = 0; y < HEIGHT; y++) {
@@ -61,6 +75,7 @@ class FloodButtons extends Command {
       return str;
     }
 
+    // Get the content of the embed
     async function getContent() {
       let embed;
       if (gameOver === true) {
@@ -82,7 +97,8 @@ class FloodButtons extends Command {
         let highScoreUser;
         let highScoreTime;
         if (result === 'winner') {
-          const HS = { score: turn, user: msg.author.tag, time: gameTimeSeconds };
+          const authorName = msg.author.discriminator === '0' ? msg.author.username : msg.author.tag;
+          const HS = { score: turn, user: authorName, time: gameTimeSeconds };
           const oldHS = await db.get('global.highScores.flood');
           highScore = oldHS?.score || 0;
           highScoreUser = oldHS?.user || 'N/A';
@@ -105,27 +121,29 @@ class FloodButtons extends Command {
           highScoreTime = oldHS?.time || 0;
         }
 
-        if (!isNaN(highScoreTime))
+        if (!isNaN(highScoreTime)) {
           highScoreTime = Duration.fromMillis(highScoreTime * 1000)
             .shiftTo('minutes', 'seconds')
             .toHuman();
+        }
+
         embed = new EmbedBuilder()
-          .setAuthor({ name: msg.member.displayName, iconURL: msg.author.displayAvatarURL({ dynamic: true }) })
-          .setColor('#08b9bf')
+          .setAuthor({ name: msg.member.displayName, iconURL: msg.author.displayAvatarURL() })
+          .setColor(color)
           .setTitle('Flood')
           .setDescription(`${gameBoardToString()} \nGame Over! \n${turnResp[result]}`)
           .addFields([{ name: 'High Score', value: `${highScore} turns by ${highScoreUser} in ${highScoreTime}` }])
           .setTimestamp();
       } else {
         embed = new EmbedBuilder()
-          .setAuthor({ name: msg.member.displayName, iconURL: msg.author.displayAvatarURL({ dynamic: true }) })
-          .setColor('#08b9bf')
+          .setAuthor({ name: msg.member.displayName, iconURL: msg.author.displayAvatarURL() })
+          .setColor(color)
           .setTitle('Flood')
           .setDescription(
             `${gameBoardToString()} 
-Fill the entire image with the same color in 25 or fewer flood tiles (turns).
+Fill the entire image with the same color in ${moves} or fewer flood tiles (turns).
 
-Click on the buttons below to fill the area above.
+Click on the reactions below to fill the area above.
 Filling starts at the top left corner.`,
           )
           .addFields([{ name: 'Turn:', value: turn.toString() }])
@@ -133,62 +151,109 @@ Filling starts at the top left corner.`,
           .setTimestamp();
       }
 
-      return [embed];
+      return embed;
+    }
+
+    function getButtons(gameOver) {
+      const redSquare = new ButtonBuilder()
+        .setCustomId(':red_square:')
+        .setLabel('Red')
+        .setEmoji('🟥')
+        .setStyle(ButtonStyle.Secondary);
+      if (gameOver) redSquare.setDisabled(true);
+      const blueSquare = new ButtonBuilder()
+        .setCustomId(':blue_square:')
+        .setLabel('Blue')
+        .setEmoji('🟦')
+        .setStyle(ButtonStyle.Secondary);
+      if (gameOver) blueSquare.setDisabled(true);
+      const orangeSquare = new ButtonBuilder()
+        .setCustomId(':orange_square:')
+        .setLabel('Orange')
+        .setEmoji('🟧')
+        .setStyle(ButtonStyle.Secondary);
+      if (gameOver) orangeSquare.setDisabled(true);
+      const purpleSquare = new ButtonBuilder()
+        .setCustomId(':purple_square:')
+        .setLabel('Purple')
+        .setEmoji('🟪')
+        .setStyle(ButtonStyle.Secondary);
+      if (gameOver) purpleSquare.setDisabled(true);
+      const greenSquare = new ButtonBuilder()
+        .setCustomId(':green_square:')
+        .setLabel('Green')
+        .setEmoji('🟩')
+        .setStyle(ButtonStyle.Secondary);
+      if (gameOver) greenSquare.setDisabled(true);
+      const octagonalSign = new ButtonBuilder()
+        .setCustomId(':octagonal_sign:')
+        .setLabel('Stop')
+        .setEmoji('🛑')
+        .setStyle(ButtonStyle.Secondary);
+      if (gameOver) octagonalSign.setDisabled(true);
+
+      const row1 = new ActionRowBuilder().addComponents(redSquare, blueSquare, orangeSquare, purpleSquare, greenSquare);
+      const row2 = new ActionRowBuilder().addComponents(octagonalSign);
+
+      return [row1, row2];
     }
 
     try {
-      // New button collector
-      const row = new ActionRowBuilder().addComponents(
-        new ButtonBuilder().setCustomId(':red_square:').setLabel('Red').setEmoji('🟥').setStyle('Secondary'),
-        new ButtonBuilder().setCustomId(':blue_square:').setLabel('Blue').setEmoji('🟦').setStyle('Secondary'),
-        new ButtonBuilder().setCustomId(':orange_square:').setLabel('Orange').setEmoji('🟧').setStyle('Secondary'),
-        new ButtonBuilder().setCustomId(':purple_square:').setLabel('Purple').setEmoji('🟪').setStyle('Secondary'),
-        new ButtonBuilder().setCustomId(':green_ square:').setLabel('Green').setEmoji('🟩').setStyle('Secondary'),
-      );
+      const embed = await getContent();
+      message = await msg.channel.send({ embeds: [embed], components: getButtons(gameOver) });
 
-      message = await msg.channel.send({ embeds: getContent(), components: [row] });
+      while (!gameOver && turn < moves) {
+        const current = gameBoard[0];
+        const queue = [{ x: 0, y: 0 }];
+        const visited = [];
+        let selected = null;
 
-      if (message) {
-        message.edit({ embeds: getContent(), components: [row] });
-      }
+        const embed = await getContent();
+        message.edit({ embeds: [embed] });
 
-      const current = gameBoard[0];
-      const queue = [{ x: 0, y: 0 }];
-      const visited = [];
-      let selected = null;
+        collected = await message
+          .awaitMessageComponent({
+            filter: (i) => i.deferUpdate(),
+            time: 60_000,
+          })
+          .catch(() => {});
 
-      const filter = (interaction) => {
-        if (interaction.user.id === msg.author.id) return true;
-        interaction.reply({ content: "You can't use this button.", ephemeral: true });
-      };
-
-      const collector = await message.createMessageComponentCollector({ filter, max: 25 });
-
-      collector.on('end', async (collected) => {
-        if (collected.size === 0) {
+        if (!collected) {
           gameOver = true;
           result = 'timeOut';
           this.client.games.delete(msg.channel.id);
-          return message.edit({ embeds: getContent(), components: [row] });
-        }
-        if (collected.deferred === false) {
-          await collected.deferUpdate();
-        }
-        selected = collected.customId;
+          const embed = await getContent();
 
-        /*
+          return message.edit({ embeds: [embed], components: getButtons(gameOver) });
+        }
+
+        const interactionUser = collected.user;
+        if (interactionUser.id !== msg.author.id) {
+          await collected.editReply({ content: `These buttons aren't for you!`, ephemeral: true }).catch(() => {});
+          continue;
+        }
+
+        const customId = collected.customId;
+        selected = nameToEmoji[customId];
+
         if (selected === '🛑') {
           gameOver = true;
           result = 'earlyEnd';
           this.client.games.delete(msg.channel.id);
-          return message.edit({ embeds: getContent(), components: [row] });
-        }
-        */
+          const embed = await getContent();
 
-        // while (gameOver === false && turn < 25) {
-        console.log(turn);
-        console.log(queue.length);
+          return collected.editReply({ embeds: [embed], components: getButtons(gameOver) });
+        } else if (selected === lastMove) {
+          await collected.editReply({
+            content: "You can't flood with the same color twice in a row!",
+            ephemeral: true,
+          });
+          continue;
+        }
+
+        lastMove = selected;
         turn += 1;
+
         while (queue.length > 0) {
           const pos = queue.shift();
           if (!pos || visited.includes(pos)) {
@@ -219,6 +284,7 @@ Filling starts at the top left corner.`,
         gameOver = true;
         result = 'winner';
         gameEnd = Date.now();
+
         for (let y = 0; y < HEIGHT; y++) {
           for (let x = 0; x < WIDTH; x++) {
             if (gameBoard[y * WIDTH + x] !== selected) {
@@ -227,27 +293,34 @@ Filling starts at the top left corner.`,
             }
           }
         }
-        // }
-      });
-
-      if (gameOver === true) {
-        this.client.games.delete(msg.channel.id);
-        return message.edit({ embeds: getContent() });
       }
 
-      if (turn >= 25) {
+      if (gameOver) {
+        this.client.games.delete(msg.channel.id);
+        const embed = await getContent();
+
+        return collected.editReply({ embeds: [embed], components: getButtons(gameOver) });
+      }
+
+      if (turn >= moves) {
         this.client.games.delete(msg.channel.id);
         gameOver = true;
         result = 'maxTurns';
-        return message.edit({ embeds: getContent() });
+        const embed = await getContent();
+
+        return collected.editReply({ embeds: [embed], components: getButtons(gameOver) });
       }
+
+      this.client.games.delete(msg.channel.id);
+      return msg.channel.send('Something went wrong, please try again later.');
     } catch (err) {
       this.client.games.delete(msg.channel.id);
-
-      console.error(err);
+      this.client.logger.error(`Flood: ${err}`);
       gameOver = true;
       result = 'error';
-      return message.edit({ embeds: getContent() });
+      const embed = await getContent();
+
+      return message.edit({ embeds: [embed], components: getButtons(gameOver) });
     }
   }
 }
