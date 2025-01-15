9b28c0ed0e9746ecc6b92e7892193006e751cb05
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2025-01-15 16:56:02 -0600

    Update ping commands again

--- slash_commands/Information/ping.js
+++ slash_commands/Information/ping.js
@@ -10,7 +10,7 @@ exports.run = async (interaction) => {
   await interaction.deferReply();
   const reply = await interaction.editReply('Loading data...');
   await interaction.editReply(
-    `🏓 API Latency is ${reply.createdTimestamp - interaction.createdTimestamp}ms. Latency is ${Math.round(
+    `🏓 Bot Latency is ${reply.createdTimestamp - interaction.createdTimestamp}ms. API Latency is ${Math.round(
       interaction.client.ws.ping,
     )}ms.`,
   );
