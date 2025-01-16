e5a010f6cda9b17418983b545a871681c360694e
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2025-01-16 16:36:02 -0600

    Fix reset-money

--- commands/Economy/reset-money.js
+++ commands/Economy/reset-money.js
@@ -16,18 +16,20 @@ class ResetMoney extends Command {
 
   async run(msg, args, level) {
     async function resetBalance(msg, mem) {
-      const amount = (await db.get(`servers.${msg.guild.id}.economy.startBalance`)) || 0;
+      const amount = (await db.get(`servers.${msg.guild.id}.economy.startBalance`)) || '0';
       await db.set(`servers.${msg.guild.id}.users.${mem.id}.economy.cash`, amount);
-      await db.set(`servers.${msg.guild.id}.users.${mem.id}.economy.bank`, 0);
+      await db.set(`servers.${msg.guild.id}.users.${mem.id}.economy.bank`, '0');
       return true;
     }
 
     if (!args || args.length < 1) {
-      await msg.channel.send('Are you sure you want to reset your money? (yes/no)');
+      const reply = await msg.channel.send('Are you sure you want to reset your money? (yes/no)');
       const verification = await this.client.util.verify(msg.channel, msg.author);
       if (verification) {
+        await reply.edit('Resetting money...');
         await resetBalance(msg, msg.member);
-        return msg.channel.send('Your money has been reset.');
+        await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait 5 seconds
+        return await reply.edit('Your money has been reset.');
       } else {
         return msg.channel.send('Cancelled, your money will not be reset.');
       }
@@ -50,11 +52,13 @@ This command requires level ${this.client.levelCache.Administrator} (Administrat
       }
       mem = mem.user ? mem.user : mem;
 
-      await msg.channel.send(`Are you sure you want to reset ${mem.tag}'s money? (yes/no)`);
+      const reply = await msg.channel.send(`Are you sure you want to reset ${mem.tag}'s money? (yes/no)`);
       const verification = await this.client.util.verify(msg.channel, msg.author);
       if (verification) {
+        await reply.edit('Resetting money...');
         await resetBalance(msg, mem);
-        return msg.channel.send(`Successfully reset ${mem.tag}'s money.`);
+        await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait 5 seconds
+        return await reply.edit(`Successfully reset ${mem.tag}'s money.`);
       } else {
         return msg.channel.send(`Cancelled, ${mem.tag}'s money won't be reset.`);
       }
